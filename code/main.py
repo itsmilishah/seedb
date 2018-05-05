cc0bf9174d78e7556d4de8bbce57444f05f99ab6
surya1495 <9112765+suryatejadev@users.noreply.github.com>
2018-05-05 05:08:25 -0400

    Minor changes

--- code/main.py
+++ code/main.py
@@ -4,24 +4,29 @@ from db_utils import *
 
 def main(db_name):
     seedb = SeeDB(db_name)
-    # convert query to float test
-    query = 'select avg(capital_gain) \
-            from census group by workclass \
-            order by workclass limit 10;'
-    a = select_query(seedb.db, query, return_float=True)
-    print('query float outputs = ',a)
-    
-    # kl divergence test
-    p1 = np.array(([1,2,3,4,5]))
-    p2 = np.array(([4,1,2,6,5]))
-    print('kl divg = ',kl_divergence(p1,p2))
+    #  # convert query to float test
+    #  query = 'select avg(capital_gain) \
+    #          from census group by workclass \
+    #          order by workclass limit 10;'
+    #  a = select_query(seedb.db, query, return_float=True)
+    #  print('query float outputs = ',a)
+    #
+    #  # kl divergence test
+    #  p1 = np.array(([1,2,3,4,5]))
+    #  p2 = np.array(([4,1,2,6,5]))
+    #  print('kl divg = ',kl_divergence(p1,p2))
+    #
+    #  # pruning test
+    #  kl_divg = np.array(([7,3,4,2.5,1,5,6,3.5,8,2.1]))
+    #  iter_phase = 1
+    #  N_phase = 10
+    #  a = seedb.prune(kl_divg, iter_phase, N_phase)
+    #  print('views to be removed = ',a)
 
-    # pruning test
-    kl_divg = np.array(([7,3,4,2.5,1,5,6,3.5,8,2.1]))
-    iter_phase = 1
-    N_phase = 10
-    a = seedb.prune(kl_divg, iter_phase, N_phase)
-    print('views to be removed = ',a)
+    # test visualization
+    views = [('workclass','age','avg'), ('sex','capital_gain','avg')]
+    labels = ['married','unmarried']
+    seedb.visualize(views, labels)
 
 if __name__ == '__main__':
     
--- code/seedb.py
+++ code/seedb.py
@@ -1,5 +1,6 @@
 from typing import List
 from db_utils import *
+import pdb
 
 class SeeDB(object):
     '''
@@ -56,7 +57,7 @@ class SeeDB(object):
                 ## Sharing optimization : combine multiple aggregates
                 selections = ''
                 for measure in candidate_views[attributes]:
-                    for view in candidate_views[attributes][measure]
+                    for view in candidate_views[attributes][measure]:
                         selections += func + '(' + measure + '), '
                 selections = selections[: -3] # removes ', ' in the end
                 query_dataset_query = self._make_view_query(selections,
@@ -92,19 +93,62 @@ class SeeDB(object):
 
         raise NotImplementedError
 
-    def _make_view_query(selections, table_name, cond, attribute,
+    def _make_view_query(self, selections, table_name, cond, attribute,
             start, end):
-        return ' '.join(['select', query,
+        return ' '.join(['select', selections,
                         'from', table_name,
-                        'where', query_dataset_cond,
+                        'where', cond,
                         'and id >= ', start, 'id < ', end,
                         'group by', attribute,
                         'order by', attribute])
 
 
-    def visualize(self, measure_name: str, function_name: str,
-            attribute_name: str, attribute_values: List) -> None:
-        raise NotImplementedError
+    def visualize(self, views, labels=None) -> None:
+        '''
+
+        '''
+        query_dataset_cond = "marital_status in ('Married-civ-spouse', 'Married-spouse-absent', 'Married-AF-spouse')" 
+        reference_dataset_cond = "marital_status in ('Divorced', 'Never-married', 'Separated', 'Widowed')"
+        if labels==None:
+            labels = ['Query','Reference']
+        for view in views:
+            attribute, measure, function = view
+            # get the query table result
+            selection = function + '(' + measure + '), '
+            query_dataset_query = self._make_view_query(selection,
+                    self.table_name, query_dataset_cond, attribute, 0, self.n_tuples)
+            reference_dataset_query = self._make_view_query(selection,
+                    self.table_name, reference_dataset_cond, attribute, 0, self.n_tuples)
+            table_query = np.array(select_query(self.db, query_dataset_query))
+            table_reference = np.array(select_query(self.db, reference_dataset_query))
+            
+            # create plot
+            plt.figure()
+            n_groups = table.shape[0]
+            index = np.arange(n_groups)
+            bar_width = 0.35
+            opacity = 0.8
+             
+            rects1 = plt.bar(index, table_query[:,1], bar_width,
+                             alpha=opacity,
+                             color='b',
+                             label=labels[0])
+             
+            rects2 = plt.bar(index + bar_width, table_reference[:,1], bar_width,
+                             alpha=opacity,
+                             color='g',
+                             label=labels[1])
+             
+            plt.xlabel(attribute)
+            plt.ylabel(function+'('+measure+')')
+            plt.title('View = ',view)
+            plt.xticks(index + bar_width, tuple(table_query[:,0]))
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig(attribute+'_'+measure+'_'+function+'.png', dpi=300)
+            plt.close()
+        pdb.set_trace()
+
 
     def phase_idx_generator(self, n_phases=10):
         '''
